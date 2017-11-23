9e65a38a7a6c678c6a2cba1e7c116c29e438500a
rlawlgns077 <rlawlgns077@gamil.com>
2017-11-24 02:13:51 +0900

    init

---
+++ src/test/java/com/ridecrew/springbootridecrew/SpringBootRidecrewApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ridecrew.springbootridecrew;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootRidecrewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
