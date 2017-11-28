f99d8123b341e452f2a09a352805363f0339e88d
KIM <rlawlgns077@gmail.com>
2017-11-28 14:08:12 +0900

    add mysql

--- src/main/java/com/ridecrew/springbootridecrew/HelloRestController.java
+++ src/main/java/com/ridecrew/springbootridecrew/HelloRestController.java
@@ -1,12 +1,17 @@
 package com.ridecrew.springbootridecrew;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRestController {
+	@Autowired
+    private BlogDao blogDao;
+	
 	@RequestMapping("/")
-	public String index() {
-		return "HelloWorld Ridecrew!";
-	}
+    public String index() {
+        blogDao.insertNewContent("H한글저장가능??");
+        return "Greetings from Spring Boot!";
+    }
 }
